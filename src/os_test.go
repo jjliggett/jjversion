00139894f1922a4c788771891aa39b863dd131a7
jjliggett <67353173+jjliggett@users.noreply.github.com>
2021-09-21 22:52:45 -0400

    test: Improve tests and add more tests (#9)

---
+++ src/os_test.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestGetWD(t *testing.T) {
+	expectedDir, expectedErr := os.Getwd()
+	oss := osService{}
+	dir, err := oss.getwd()
+	if dir != expectedDir || err != expectedErr {
+		t.Fatalf(`osService.getwd() = %q, %#v, want match for %q, %#v`, dir, err, expectedDir, expectedErr)
+	}
+}
--- src/version_test.go
+++ src/version_test.go
@@ -18,6 +18,38 @@ func TestGetVersionSuccess(t *testing.T) {
 	text := "v0.42.2"
 	result, success := GetVersion(text)
 	if success == false || result.Major != 0 || result.Minor != 42 || result.Patch != 2 {
-		t.Fatalf(`GetVersion(text) = %q %v, want match for Version { 0, 42, 2 }, nil`, result, true)
+		t.Fatalf(`GetVersion(text) = %#v %v, want match for version {0, 42, 2}, true`, result, success)
+	}
+}
+
+func TestIncrementMajorInitialVersion(t *testing.T) {
+	v := version{0, 2, 1}
+	v.IncrementMajor()
+	if v.Major != 0 || v.Minor != 3 || v.Patch != 0 {
+		t.Fatalf(`version {0, 2, 1} .IncrementMajor() = %#v, want match for version {0, 3, 0}`, v)
+	}
+}
+
+func TestIncrementMajor(t *testing.T) {
+	v := version{1, 2, 3}
+	v.IncrementMajor()
+	if v.Major != 2 || v.Minor != 0 || v.Patch != 0 {
+		t.Fatalf(`version {1, 2, 3} .IncrementMajor() = %#v, want match for version {2, 0, 0}`, v)
+	}
+}
+
+func TestIncrementMinor(t *testing.T) {
+	v := version{1, 2, 3}
+	v.IncrementMinor()
+	if v.Major != 1 || v.Minor != 3 || v.Patch != 0 {
+		t.Fatalf(`version {1, 2, 3} .IncrementMinor() = %#v, want match for version {1, 3, 0}`, v)
+	}
+}
+
+func TestIncrementPatch(t *testing.T) {
+	v := version{1, 2, 3}
+	v.IncrementPatch()
+	if v.Major != 1 || v.Minor != 2 || v.Patch != 4 {
+		t.Fatalf(`version {1, 2, 3} .IncrementPatch() = %#v, want match for version {1, 2, 4}`, v)
 	}
 }
